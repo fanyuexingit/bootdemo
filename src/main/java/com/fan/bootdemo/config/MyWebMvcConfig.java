1cc2c2ba945bee6108ccb779dfc2192bde634c43
fanyuexin <fanyuexin@e-nci.com>
2021-02-24 17:39:13 +0800

    springmvc

---
+++ src/main/java/com/fan/bootdemo/config/MyWebMvcConfig.java
@@ -0,0 +1,37 @@
+/**
+ * @description:
+ * @author: Andy
+ * @time: 2021/02/24 16:27
+ */
+package com.fan.bootdemo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.View;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.Locale;
+
+/**
+ * Created by fanyuexin on 2021/02/24
+ */
+@Configuration
+public class MyWebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/qqq").setViewName("test");
+    }
+
+    @Bean
+    public ViewResolver getViewResolver(){
+        return new ViewResolver() {
+            @Override
+            public View resolveViewName(String viewName, Locale locale) throws Exception {
+                return null;
+            }
+        };
+    }
+}
