72b804bb147a7edd38097ebc5f541457c78e70b9
fanyuexin <fanyuexin@e-nci.com>
2021-03-09 00:04:51 +0800

    cache

---
+++ bootcache/src/test/java/com/example/bootcache/CacheTest.java
@@ -0,0 +1,27 @@
+package com.example.bootcache;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CacheTest {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    public void test() throws Exception {
+        // 创建1条记录
+        userRepository.save(new User("AAA", 10));
+
+        User u1 = userRepository.findByName("AAA");
+        System.out.println("第一次查询：" + u1.getAge());
+
+        User u2 = userRepository.findByName("AAA");
+        System.out.println("第二次查询：" + u2.getAge());
+    }
+}
